421995138d3083b1394734dc70d6b991e4617986
Ravi Chauhan <ravi87104@gmail.com>
2023-03-14 17:10:23 +0530

    link change

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -6,7 +6,7 @@ declare var $: any;
   providedIn: 'root'
 })
 export class ApiService {
-  public static HOST_URL: string = "http://localhost:9000";
+  public static HOST_URL: string = "https://bapsanandmandir.co.in";
   // public static HOST_URL: string = "http://31.220.111.153:5555";
   toasts: any[] = [];
   constructor(
